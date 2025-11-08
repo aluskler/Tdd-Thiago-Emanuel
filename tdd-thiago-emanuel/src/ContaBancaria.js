c53e93608ee2ee58913b9385761e71f338ad20de
Thiago Emanuel <thiagoemanoelbf@hotmail.com>
2025-11-07 21:47:10 -0300

    Implementar método depositar

--- tdd-thiago-emanuel/src/ContaBancaria.js
+++ tdd-thiago-emanuel/src/ContaBancaria.js
@@ -6,4 +6,11 @@ export default class ContaBancaria {
   getSaldo() {
     return this.saldo;
   }
+
+  depositar(valor) {
+    if (typeof valor !== "number" || valor <= 0) {
+      throw new Error("Valor de depósito inválido");
+    }
+    this.saldo += valor;
+  }
 }
